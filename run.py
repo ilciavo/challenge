78bcb747e767559395be42656954f0e5fc211cff
Leo <leonardo.echeverria@erdw.ethz.ch>
2020-10-08 17:20:24 +0200

    initializing repo

---
+++ run.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, abort
+from dataclasses import dataclass
+import json
+
+@dataclass
+class Document:
+    doc_id: int
+    text: str
+
+class Summary:
+    sum_id: int
+    summary: str
+
+class DocumentService:
+    def __init__(self):
+        self.db = dict()
+        self.next_id = 0
+
+    def retrieve(self, doc_id: int):
+        return self.db.get(int(doc_id))
+
+    def store(self, text: str):
+        self.next_id += 1
+        doc = Document(self.next_id, text)
+        self.db[doc.doc_id] = doc
+        return doc
+
+    def summarize(self, sum_id:int):
+        text = self.db[sum_id].text
+        #s_text = summarize(text)
+        s_text = 'this is a dummy summary'
+        summary = Summary(sum_id, s_text)
+        return summary
+
+app = Flask(__name__)
+
+service = DocumentService()
+
+@app.route('/', methods = ['POST'])
+def store_document():
+    text = request.form['text']
+    doc = service.store(text)
+    return doc.__dict__
+
+@app.route('/', methods = ['GET'])
+def retrieve_document():
+    doc_id = request.args.get('doc_id')
+    doc = service.retrieve(doc_id)
+    if doc is not None:
+        return doc.__dict__
+    else:
+        abort(404)
+
+@app.route('/', methods = ['GET'])
+def get_summary():
+    sum_id = request.args.get('doc_id')
+    summary = service.summarize(sum_id)
+    if summary is not None:
+        return summary.__dict__
+    else:
+        abort(404)
+
+if __name__ == '__main__':
+    app.run()
---
+++ test.py
@@ -0,0 +1,35 @@
+from run import app
+import unittest
+import json
+
+class FlaskTest(unittest.TestCase):
+    def test_store(self):
+        request_data = dict(text='this is a very long test to store')
+        tester = app.test_client(self)
+        response = tester.post('/', data=request_data)
+        data = response.get_data()
+        print('stored data:', data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve(self):
+        dummy_data = dict(text='this is a very long test to retrieve')
+        request_data = dict(doc_id='1')
+        tester = app.test_client(self)
+        tester.post('/', data=dummy_data)
+        response = tester.get('/', query_string=request_data)
+        data = response.get_data()
+        print('retrieved data:', data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_summarize(self):
+        dummy_data = dict(text='this is a very long test to summarize')
+        request_data = dict(doc_id='1')
+        tester = app.test_client(self)
+        tester.post('/', data=dummy_data)
+        response = tester.get('/', query_string=request_data)
+        data = response.get_data()
+        print('summarized data:', data)
+        #self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
